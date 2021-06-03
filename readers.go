5e072a59f44d22a7f7651172edb5f3872ca2160c
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-06-03 11:46:19 -0600

    Implementing various readers.

--- readers.go
+++ readers.go
@@ -1,1 +1,43 @@
 package shuttle
+
+import "net/http"
+
+// acceptReader
+
+// deserializeReader
+
+// bindReader
+
+type bindReader struct {
+	factory func(error) interface{}
+}
+
+func newBindReader(factory func(error) interface{}) Reader {
+	return &bindReader{factory: factory}
+}
+
+func (this *bindReader) Read(target InputModel, request *http.Request) interface{} {
+	if err := target.Bind(request); err != nil {
+		return this.factory(err)
+	}
+
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type validationReader struct {
+	buffer []error
+}
+
+func newValidationReader(bufferSize int) Reader {
+	return &validationReader{buffer: make([]error, bufferSize)}
+}
+
+func (this *validationReader) Read(target InputModel, _ *http.Request) interface{} {
+	if count := target.Validate(this.buffer); count > 0 {
+		return this.buffer[0:count]
+	}
+
+	return nil
+}
--- readers_test.go
+++ readers_test.go
@@ -1,1 +1,69 @@
 package shuttle
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestBindReader_NoErrors(t *testing.T) {
+	input := &TestInputModel{}
+	request := httptest.NewRequest("GET", "/", nil)
+
+	result := newBindReader(nil).Read(input, request)
+
+	Assert(t).That(result).IsNil()
+	Assert(t).That(input.boundRequest == request).IsTrue()
+}
+func TestBindReader_Error(t *testing.T) {
+	var bindError error
+	input := &TestInputModel{bindError: errors.New("bind error")}
+	request := httptest.NewRequest("GET", "/", nil)
+
+	result := newBindReader(func(err error) interface{} {
+		bindError = err
+		return "result"
+	}).Read(input, request)
+
+	Assert(t).That(result).Equals("result")
+	Assert(t).That(bindError).Equals(input.bindError)
+}
+
+func TestValidateReader_NoErrors(t *testing.T) {
+	input := &TestInputModel{}
+
+	result := newValidationReader(4).Read(input, nil)
+
+	Assert(t).That(result).IsNil()
+}
+func TestValidateReader_ErrorResult(t *testing.T) {
+	input := &TestInputModel{
+		validationErrors: []error{errors.New("1"), errors.New("2")},
+	}
+
+	result := newValidationReader(4).Read(input, nil)
+
+	Assert(t).That(result).Equals(input.validationErrors)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type TestInputModel struct {
+	boundRequest     *http.Request
+	bindError        error
+	validationErrors []error
+}
+
+func (this *TestInputModel) Reset() {}
+func (this *TestInputModel) Bind(request *http.Request) error {
+	this.boundRequest = request
+	return this.bindError
+}
+func (this *TestInputModel) Validate(errs []error) int {
+	for i := range this.validationErrors {
+		errs[i] = this.validationErrors[i]
+	}
+
+	return len(this.validationErrors)
+}
