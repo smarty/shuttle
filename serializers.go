fb11f86a35c55b55b266987e6c780bd4907a0445
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-06-03 16:22:50 -0600

    Deserialization implementation.

--- serializers.go
+++ serializers.go
@@ -1,1 +1,27 @@
 package shuttle
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type jsonDeserializer struct {
+	decoder *json.Decoder
+	source  struct{ io.Reader }
+}
+
+func newJSONDeserializer() Deserializer {
+	this := &jsonDeserializer{}
+	this.decoder = json.NewDecoder(&this.source)
+	return this
+}
+
+func (this *jsonDeserializer) Deserialize(target interface{}, source io.Reader) error {
+	this.source.Reader = source
+
+	if err := this.decoder.Decode(target); err != nil {
+		return ErrDeserializationFailure
+	}
+
+	return nil
+}
--- serializers_test.go
+++ serializers_test.go
@@ -1,1 +1,23 @@
 package shuttle
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestJSONDeserializer(t *testing.T) {
+	var value string
+	deserializer := newJSONDeserializer()
+	err := deserializer.Deserialize(&value, bytes.NewBufferString(`"hello"`))
+
+	Assert(t).That(err).IsNil()
+	Assert(t).That(value).Equals("hello")
+}
+func TestJSONDeserializer_ReturnError(t *testing.T) {
+	var value string
+	deserializer := newJSONDeserializer()
+	err := deserializer.Deserialize(&value, bytes.NewBufferString(`{`))
+
+	Assert(t).That(err).Equals(ErrDeserializationFailure)
+	Assert(t).That(value).Equals("")
+}
