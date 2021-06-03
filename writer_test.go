d2361d976a96f620c422ba3dc2ea26ddfc962441
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-06-03 08:58:20 -0600

    Asserting and implementing behavior of default writer.

--- writer_test.go
+++ writer_test.go
@@ -1,6 +1,8 @@
 package shuttle
 
 import (
+	"bytes"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
@@ -10,16 +12,48 @@ func TestWrite(t *testing.T) {
 		Input interface{}
 		HTTPResponse
 	}{
-		{Input: nil, HTTPResponse: HTTPResponse{
-			StatusCode: 200, ContentType: nil, Body: ""}},
-		{Input: "body", HTTPResponse: HTTPResponse{
-			StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "body"}},
-		{Input: []byte("body"), HTTPResponse: HTTPResponse{
-			StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "body"}},
-		{Input: true, HTTPResponse: HTTPResponse{
-			StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "true"}},
-		{Input: false, HTTPResponse: HTTPResponse{
-			StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "false"}},
+		{Input: nil,
+			HTTPResponse: HTTPResponse{StatusCode: 200, ContentType: nil, Body: ""}},
+		{Input: "body",
+			HTTPResponse: HTTPResponse{StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "body"}},
+		{Input: []byte("body"),
+			HTTPResponse: HTTPResponse{StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "body"}},
+
+		{Input: true,
+			HTTPResponse: HTTPResponse{StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "true"}},
+		{Input: false,
+			HTTPResponse: HTTPResponse{StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "false"}},
+
+		{Input: &TextResult{StatusCode: 0, ContentType: "", Content: "body"},
+			HTTPResponse: HTTPResponse{StatusCode: 200, ContentType: []string{"text/plain; charset=utf-8"}, Body: "body"}},
+		{Input: &TextResult{StatusCode: 201, ContentType: "application/custom", Content: "body"},
+			HTTPResponse: HTTPResponse{StatusCode: 201, ContentType: []string{"application/custom"}, Body: "body"}},
+		{Input: &TextResult{StatusCode: 201, ContentType: "no-expected-content-type-for-empty-body", Content: ""},
+			HTTPResponse: HTTPResponse{StatusCode: 201, ContentType: nil, Body: ""}},
+		{Input: TextResult{StatusCode: 0, ContentType: "application/custom", Content: "body"},
+			HTTPResponse: HTTPResponse{StatusCode: 200, ContentType: []string{"application/custom"}, Body: "body"}},
+		{Input: TextResult{StatusCode: 202, ContentType: "", Content: "body"},
+			HTTPResponse: HTTPResponse{StatusCode: 202, ContentType: nil, Body: "body"}},
+		{Input: TextResult{StatusCode: 201, ContentType: "no-expected-content-type-for-empty-body", Content: ""},
+			HTTPResponse: HTTPResponse{StatusCode: 201, ContentType: nil, Body: ""}},
+
+		{Input: &BinaryResult{StatusCode: 404, ContentType: "", Content: []byte("body")},
+			HTTPResponse: HTTPResponse{StatusCode: 404, ContentType: nil, Body: "body"}},
+		{Input: BinaryResult{StatusCode: 404, ContentType: "", Content: []byte("body")},
+			HTTPResponse: HTTPResponse{StatusCode: 404, ContentType: nil, Body: "body"}},
+		{Input: &BinaryResult{StatusCode: 404, ContentType: "no-expected-content-type-for-empty-body", Content: nil},
+			HTTPResponse: HTTPResponse{StatusCode: 404, ContentType: nil, Body: ""}},
+		{Input: BinaryResult{StatusCode: 404, ContentType: "no-expected-content-type-for-empty-body", Content: nil},
+			HTTPResponse: HTTPResponse{StatusCode: 404, ContentType: nil, Body: ""}},
+
+		{Input: &StreamResult{StatusCode: 404, ContentType: "", Content: bytes.NewBufferString("body")},
+			HTTPResponse: HTTPResponse{StatusCode: 404, ContentType: nil, Body: "body"}},
+		{Input: StreamResult{StatusCode: 422, ContentType: "application/custom", Content: bytes.NewBufferString("body")},
+			HTTPResponse: HTTPResponse{StatusCode: 422, ContentType: []string{"application/custom"}, Body: "body"}},
+		{Input: &StreamResult{StatusCode: 404, ContentType: "no-expected-content-type-for-empty-body", Content: nil},
+			HTTPResponse: HTTPResponse{StatusCode: 404, ContentType: nil, Body: ""}},
+		{Input: StreamResult{StatusCode: 404, ContentType: "no-expected-content-type-for-empty-body", Content: nil},
+			HTTPResponse: HTTPResponse{StatusCode: 404, ContentType: nil, Body: ""}},
 	}
 
 	for _, assertion := range assertions {
@@ -45,8 +79,31 @@ func assertResponse(t *testing.T, response *httptest.ResponseRecorder, expected 
 	Assert(t).That(response.Body.String()).Equals(expected.Body)
 }
 
+func TestWriteHTTPHandler(t *testing.T) {
+	handler := &TestHTTPHandlerResult{}
+	response := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/", nil)
+
+	newTestWriter(nil).Write(response, request, handler)
+
+	Assert(t).That(handler.response == response).IsTrue()
+	Assert(t).That(handler.request == request).IsTrue()
+}
+
 type HTTPResponse struct {
 	StatusCode  int
 	ContentType []string
 	Body        string
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type TestHTTPHandlerResult struct {
+	response http.ResponseWriter
+	request  *http.Request
+}
+
+func (this *TestHTTPHandlerResult) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	this.response = response
+	this.request = request
 }
--- writers.go
+++ writers.go
@@ -6,29 +6,99 @@ import (
 )
 
 type defaultWriter struct {
+	bodyBuffer   []byte
+	headerBuffer []string
 }
 
 func newWriter(serializers map[string]func() Serializer) Writer {
 	return &defaultWriter{
-		// TODO
+		bodyBuffer:   make([]byte, 1024*4),
+		headerBuffer: make([]string, 1),
 	}
 }
 
 func (this *defaultWriter) Write(response http.ResponseWriter, request *http.Request, result interface{}) {
 	if result == nil {
 		return
+	} else if handler, ok := result.(http.Handler); ok {
+		handler.ServeHTTP(response, request)
+	} else {
+		this.write(response, request, result)
+	}
+}
+func (this *defaultWriter) write(response http.ResponseWriter, request *http.Request, result interface{}) {
+	switch typed := result.(type) {
+	case *TextResult:
+		this.writeTextResult(response, typed)
+	case TextResult:
+		this.writeTextResult(response, &typed)
+
+	case *BinaryResult:
+		this.writeBinaryResult(response, typed)
+	case BinaryResult:
+		this.writeBinaryResult(response, &typed)
+
+	case *StreamResult:
+		this.writeStreamResult(response, typed)
+	case StreamResult:
+		this.writeStreamResult(response, &typed)
+
+	case string:
+		writeStringResult(response, typed)
+	case []byte:
+		writeByteResult(response, typed)
+	case bool:
+		writeBoolResult(response, typed)
+	}
+}
+
+func (this *defaultWriter) writeTextResult(response http.ResponseWriter, typed *TextResult) {
+	hasContent := len(typed.Content) > 0
+	this.writeHeader(response, typed.StatusCode, typed.ContentType, hasContent)
+	if hasContent {
+		_, _ = io.WriteString(response, typed.Content)
+	}
+}
+func (this *defaultWriter) writeBinaryResult(response http.ResponseWriter, typed *BinaryResult) {
+	hasContent := len(typed.Content) > 0
+	this.writeHeader(response, typed.StatusCode, typed.ContentType, hasContent)
+	if hasContent {
+		_, _ = response.Write(typed.Content)
+	}
+}
+func (this *defaultWriter) writeStreamResult(response http.ResponseWriter, typed *StreamResult) {
+	hasContent := typed.Content != nil
+	this.writeHeader(response, typed.StatusCode, typed.ContentType, hasContent)
+	if hasContent {
+		_, _ = io.CopyBuffer(response, typed.Content, this.bodyBuffer)
+	}
+}
+
+func writeStringResult(response http.ResponseWriter, typed string) {
+	if len(typed) > 0 {
+		_, _ = io.WriteString(response, typed)
+	}
+}
+func writeByteResult(response http.ResponseWriter, typed []byte) {
+	if len(typed) > 0 {
+		_, _ = response.Write(typed)
+	}
+}
+func writeBoolResult(response http.ResponseWriter, typed bool) {
+	if typed {
+		_, _ = io.WriteString(response, "true")
+	} else {
+		_, _ = io.WriteString(response, "false")
+	}
+}
+
+func (this *defaultWriter) writeHeader(response http.ResponseWriter, statusCode int, contentType string, hasContent bool) {
+	if hasContent && len(contentType) > 0 {
+		this.headerBuffer[0] = contentType
+		response.Header()["Content-Type"] = this.headerBuffer
 	}
 
-	switch typed := result.(type) {
-	case string:
-		_, _ = io.WriteString(response, typed)
-	case []byte:
-		_, _ = response.Write(typed)
-	case bool:
-		if typed {
-			_, _ = io.WriteString(response, "true")
-		} else {
-			_, _ = io.WriteString(response, "false")
-		}
+	if statusCode > 0 {
+		response.WriteHeader(statusCode)
 	}
 }
