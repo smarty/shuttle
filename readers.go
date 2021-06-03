5bfc5bb6d0fa03bf14fca9c21c671d8b6c904b4d
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-06-03 12:38:51 -0600

    Implemented the accept reader.

--- readers.go
+++ readers.go
@@ -1,12 +1,74 @@
 package shuttle
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
-// acceptReader
+type acceptReader struct {
+	acceptable map[string][]string
+	result     interface{}
+}
+
+func newAcceptReader(serializerFactories map[string]func() Serializer, result *TextResult) Reader {
+	acceptable := make(map[string][]string)
+	for acceptType, _ := range serializerFactories {
+		acceptable[acceptType] = []string{acceptType}
+	}
+
+	return &acceptReader{acceptable: acceptable, result: result}
+}
+
+func (this *acceptReader) Read(_ InputModel, request *http.Request) interface{} {
+	if normalized, found := this.findAcceptType(request.Header["Accept"]); !found {
+		return this.result
+	} else {
+		request.Header["Accept"] = normalized
+	}
+
+	return nil
+}
+func (this *acceptReader) findAcceptType(acceptTypes []string) ([]string, bool) {
+	if len(acceptTypes) == 0 {
+		return nil, true
+	}
+
+	for _, value := range acceptTypes {
+		var item string
+		for {
+			index := strings.Index(value, ",")
+			if index >= 0 {
+				item = value[0:index]
+				value = value[index+1:]
+			} else {
+				item = value
+			}
+
+			if item = strings.TrimSpace(item); item == "*/*" {
+				return nil, true // default
+			} else if types, contains := this.acceptable[normalizeContentType(item)]; contains {
+				return types, true
+			} else if index == -1 {
+				break
+			}
+		}
+	}
+
+	return nil, false
+}
+func normalizeContentType(value string) string {
+	if index := strings.Index(value, ";"); index >= 0 {
+		value = value[0:index]
+	}
+
+	return strings.TrimSpace(value)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // deserializeReader
 
-// bindReader
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type bindReader struct {
 	factory func(error) interface{}
--- readers_test.go
+++ readers_test.go
@@ -7,6 +7,50 @@ import (
 	"testing"
 )
 
+func TestAcceptReader_AcceptTypeProvided_NotFound_ReturnFailure(t *testing.T) {
+	assertAcceptReader(t, "fail", []string{"not-found"}, []string{"not-found"})
+}
+func TestAcceptReader_NoAcceptTypesProvided_NoError(t *testing.T) {
+	assertAcceptReader(t, "", nil, nil)
+}
+func TestAcceptReader_MultipleAcceptTypesProvided_Found_OverwriteAccept(t *testing.T) {
+	assertAcceptReader(t, "", []string{"not-found-1, found"}, []string{"found"})
+}
+func TestAcceptReader_MultipleComplexAcceptTypesProvided_Found_OverwriteAccept(t *testing.T) {
+	assertAcceptReader(t, "", []string{"not-found-1, found;q=0, not-found-2"}, []string{"found"})
+}
+func TestAcceptReader_WildcardAcceptTypeProvided_Found_OverwriteAccept(t *testing.T) {
+	assertAcceptReader(t, "", []string{"*/*"}, nil)
+}
+func assertAcceptReader(t *testing.T, expectedResult string, acceptTypes, acceptTypesWhenSuccessful []string) {
+	request := httptest.NewRequest("GET", "/", nil)
+	request.Header["Accept"] = acceptTypes
+	notAcceptable := &TextResult{Content: expectedResult}
+	serializers := map[string]func() Serializer{
+		"found": func() Serializer { return nil },
+	}
+
+	result := newAcceptReader(serializers, notAcceptable).Read(nil, request)
+
+	if len(expectedResult) == 0 {
+		Assert(t).That(result).IsNil()
+	} else {
+		Assert(t).That(result).Equals(notAcceptable)
+	}
+
+	if result == nil {
+		Assert(t).That(request.Header["Accept"]).Equals(acceptTypesWhenSuccessful)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func TestDeserializeReader(t *testing.T) {
+	// TODO
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 func TestBindReader_NoErrors(t *testing.T) {
 	input := &TestInputModel{}
 	request := httptest.NewRequest("GET", "/", nil)
@@ -29,6 +73,8 @@ func TestBindReader_Error(t *testing.T) {
 	Assert(t).That(result).Equals("result")
 	Assert(t).That(bindError).Equals(input.bindError)
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func TestValidateReader_NoErrors(t *testing.T) {
 	input := &TestInputModel{}
